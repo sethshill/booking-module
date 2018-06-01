6d6151ccbbf537535f5e9ab1459cec495bbb1e1b
nataliestam <natalielynnstam@gmail.com>
2018-06-01 16:57:14 -0700

    add listingId parameter to paths

--- server/index.js
+++ server/index.js
@@ -5,4 +5,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/../public')));
 
+app.get('/booking/core/listingId/:listingId', (req, res) => {
+  res.send(req.params);
+});
+
+app.get('/booking/availability/listingId/:listingId', (req, res) => {
+  res.send(req.params);
+});
+
 app.listen(3001, () => console.log('listening on port 3001!'));
