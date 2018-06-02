3da989deecf31a660a2228ec1641c261519ff724
nataliestam <natalielynnstam@gmail.com>
2018-06-02 14:48:37 -0700

    test get request for base data

--- client/index.jsx
+++ client/index.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      hi: '',
     };
   }
 
+  componentDidMount() {
+    this.getCoreData();
+  }
+
+  getCoreData() {
+    axios.get('/booking/core/listingId/1')
+      .then(response => console.log(response.data[0]))
+      .catch(error => console.log(error));
+
+    this.setState({
+      hi: 'hello',
+    });
+  }
+
+
   render() {
     return (
-      <div id="app">hello</div>
+      <div id="app">{this.state.hi}</div>
     );
   }
 }
