0ef861bb6f2a31df306741d0604e2d725896ec0a
nataliestam <natalielynnstam@gmail.com>
2018-06-01 17:27:13 -0700

    setup server for calls to database helpers

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const path = require('path');
+const db = require('../database/index.js');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/../public')));
 
 app.get('/booking/core/listingId/:listingId', (req, res) => {
-  res.send(req.params);
+  db.getCoreData(req.params.listingId, () => {
+    res.send();
+  });
 });
 
 app.get('/booking/availability/listingId/:listingId', (req, res) => {
-  res.send(req.params);
+  db.getReservationData(req.params.listingId, () => {
+    res.send();
+  });
 });
 
 app.listen(3001, () => console.log('listening on port 3001!'));
