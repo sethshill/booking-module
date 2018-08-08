76e802ab9bb62a7dd973fed9d381cdea09399e0d
Seth Shill <seth.s.shill@gmail.com>
2018-08-07 23:20:53 -0400

    generates data with autoincrement

--- database/config.js
+++ database/config.js
@@ -1,9 +1,18 @@
 // Connection to EC2 instance database
+// module.exports = {
+//   host: '18.217.220.189',
+//   database: 'project_nomad_booking',
+//   user: 'username',
+//   password: 'password',
+//   port: 3306,
+//   timeout: 60000,
+// };
+
 module.exports = {
-  host: '18.217.220.189',
+  host: 'localhost',
   database: 'project_nomad_booking',
-  user: 'username',
-  password: 'password',
+  user: 'root',
+  password: '',
   port: 3306,
   timeout: 60000,
 };
--- sample-data/dataGenerator.js
+++ sample-data/dataGenerator.js
@@ -4,16 +4,63 @@
 const faker = require('faker');
 const db = require('./dataGenHelpers');
 
-// Iterate for 100 iterations (make 100 listings)
-const numListings = 100;
-let idx = 0;
+// Set Constants
+const numListings = 10000;
+const today = new Date();
 
-// Keep track of time
-const startTime = Date.now();
+// Define functions
+const formatDate = date => `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 
-for (let i = 1; i < (numListings + 1); i += 1) {
+const makeReservation = (foreignId, todaysDate) => {
+  // assume up to 100 reservations per listing
+  const numReservationsPerListing = faker.random.number(100);
+
+  for (let j = 0; j < numReservationsPerListing; j += 1) {
+    let startDate = formatDate(todaysDate);
+    todaysDate.setDate(todaysDate.getDate() + 1);
+    let endDate = formatDate(todaysDate);
+
+    const reservation = {
+      listingId: foreignId,
+      startDate,
+      endDate,
+    };
+
+    db.insertReservation(reservation, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // console.log('Result is: ', result);
+      }
+    });
+  }
+};
+
+const makeDailyPrices = (foreignKey, currentDay) => {
+  for (let i = 0; i < 180; i++) {
+    const listingDailyPrice = {
+      listingId: foreignKey,
+      costPerNight: faker.random.number({
+        min: 0,
+        max: 500,
+        precision: 0.01,
+      }),
+      startDate: formatDate(currentDay),
+    };
+    db.insertDailyPrices(listingDailyPrice, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // placeholder
+      }
+    });
+    currentDay.setDate(currentDay.getDate() + 1);
+  }
+};
+
+// Create entries
+for (let i = 0; i < numListings; i += 1) {
   const listing = {
-    id: i,
     avgRating: faker.random.number(5),
     reviewCount: faker.random.number(100),
     maxAdults: faker.random.number(8),
@@ -34,62 +81,14 @@ for (let i = 1; i < (numListings + 1); i += 1) {
       max: 1,
       precision: 0.01,
     }),
-    additionalGuestFee: faker.random.number(100)
+    additionalGuestFee: faker.random.number(100),
   };
   db.insertListing(listing, (err, result) => {
     if (err) {
       console.log(err);
     } else {
-      // console.log('Result is: ', result);
+      makeReservation(result.insertId, today);
+      makeDailyPrices(result.insertId, today);
     }
   });
-
-  // assume up to 100 reservations per listing
-  const numReservationsPerListing = faker.random.number(100);
-  const today = new Date();
-  let startDate;
-  let endDate;
-  const formatDate = date => `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-
-  for (let j = 0; j < numReservationsPerListing; j += 1) {
-    startDate = formatDate(today);
-    today.setDate(today.getDate() + 1);
-    endDate = formatDate(today);
-
-    const reservation = {
-      listingId: i,
-      startDate,
-      endDate,
-    };
-
-    db.insertReservation(reservation, (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        // console.log('Result is: ', result);
-      }
-    });
-    const listingDailyPrice = {
-      id: idx,
-      listingId: i,
-      costPerNight: faker.random.number({
-        min: 0,
-        max: 500,
-        precision: 0.01,
-      }),
-      startDate,
-
-    };
-
-    db.insertDailyPrices(listingDailyPrice, (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        // let timePassed = Date.now() - startTime;
-        // console.log('Time passed (ms): ', timePassed);
-        // console.log('Result is: ', result);
-      }
-    });
-    idx += 1;
-  }
 }
