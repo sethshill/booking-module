21a0a4b3a7759a3c515c7a38e99adda1bba313e6
Seth Shill <seth.s.shill@gmail.com>
2018-08-22 21:53:10 -0400

    used vid tutorial and changed tablenames to add with 's'

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,43 @@
-const knex = require('knex');
-const KnexOrm = require('knex-orm');
+// const knex = require('knex');
+// const KnexOrm = require('knex-orm');
 
-const Database = new KnexOrm(knex({
+// const Database = new KnexOrm(knex({
+//   client: 'mysql',
+//   connection: {
+//     user: 'root',
+//     password: '',
+//     databased: 'project_nomad_booking',
+//   },
+//   migrations: {
+//     directory: __dirname + '/db/migrations'
+//   },
+//   seeds: {
+//     directory: __dirname + '/db/seeds',
+//   },
+// }));
+
+// module.exports = {
+//   Database,
+// };
+
+const path = require('path');
+
+module.exports = {
   client: 'mysql',
   connection: {
     user: 'root',
     password: '',
     databased: 'project_nomad_booking',
   },
-  migrations: {
-    directory: __dirname + '/db/migrations'
-  },
-  seeds: {
-    directory: __dirname + '/db/seeds',
-  },
-}));
-
-module.exports = {
-  Database,
+  migrations: { directory: path.join(__dirname, '/db/migrations') },
+  seeds: { directory: path.join(__dirname, '/db/seeds') },
 };
+
+
+
+
+
+
 
 // class Listing extends Database.Model {
 //   static get primaryKey() {
