a7eb9126d867545e65c962e045c6c0294ab469bb
Seth Shill <seth.s.shill@gmail.com>
2018-08-25 18:22:57 -0400

    populates 100,000 enries

--- db/seeds/01_listings.js
+++ db/seeds/01_listings.js
@@ -1,101 +1,97 @@
 const faker = require('faker');
+const path = require('path');
 // Set Constants
-// const numListings = 100000;
-const numListings = 1000;
-// const factor = 50;
-const today = new Date();
+const numListings = 100000;
+const subset = 1000;
+let today = new Date();
 
 // Define functions
 const formatDate = date => `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 
-// while iterations < factor
-//  make a promise for X # of iterations
-//  add promise to promiseAll
-// when all promises completed
-// complete seeding
-
-exports.seed = function (knex, Promise) {
+exports.seed = async function (knex, Promise) {
   // Deletes ALL existing entries
-  return Promise.join(
-    knex('listings').del(),
-    knex('reservations').del(),
-    knex('listing_daily_prices').del()
-    .then(() => {
-      console.log('a log to check how often this completes');
-      let listings = [];
-      for (let i = 0; i < numListings; i += 1) {
-        const listing = {
-          avg_rating: faker.random.number(5),
-          review_count: faker.random.number(100),
-          max_adults: faker.random.number(8),
-          max_children: faker.random.number(8),
-          max_infants: faker.random.number(5),
-          cleaning_fee: faker.random.number({
-            min: 0.1,
-            max: 1,
-            precision: 0.01,
-          }),
-          service_fee_perc: faker.random.number({
-            min: 0.1,
-            max: 0.3,
-            precision: 0.01,
-          }),
-          occ_tax_rate_perc: faker.random.number({
-            min: 0.1,
-            max: 0.3,
-            precision: 0.01,
-          }),
-          additional_guest_fee: faker.random.number({
-            min: 0.1,
-            max: 0.3,
-            precision: 0.01,
-          }),
-        };
-        listings.push(listing);
-      }
-      return knex('listings').insert(listings);
+  const createDbSubset = () => {
+    const listings = [];
+    for (let i = 0; i < subset; i += 1) {
+      const listing = {
+        avg_rating: faker.random.number(5),
+        review_count: faker.random.number(100),
+        max_adults: faker.random.number(8),
+        max_children: faker.random.number(8),
+        max_infants: faker.random.number(5),
+        cleaning_fee: faker.random.number({
+          min: 0.1,
+          max: 1,
+          precision: 0.01,
+        }),
+        service_fee_perc: faker.random.number({
+          min: 0.1,
+          max: 0.3,
+          precision: 0.01,
+        }),
+        occ_tax_rate_perc: faker.random.number({
+          min: 0.1,
+          max: 0.3,
+          precision: 0.01,
+        }),
+        additional_guest_fee: faker.random.number({
+          min: 0.1,
+          max: 0.3,
+          precision: 0.01,
+        }),
+      };
+      listings.push(listing);
     }
-  )
-  .then(() => {
-    let reservations = [];
-    for (let i = 1; i < numListings + 1; i += 1) {
-      // assume up to 10 reservations per listing
-      const numReservationsPerListing = faker.random.number(10);
-      // const numReservationsPerListing = 0;
-      for (let j = 0; j < numReservationsPerListing; j += 1) {
-        let start_date = formatDate(today);
-        today.setDate(today.getDate() + 1);
-        let end_date = formatDate(today);
+  return knex('listings').insert(listings)
+      .then(() => {
+        const reservations = [];
+        for (let i = 1; i < subset + 1; i += 1) {
+          // assume up to 10 reservations per listing
+          const numReservationsPerListing = faker.random.number(10);
+          // const numReservationsPerListing = 0;
+          for (let j = 0; j < numReservationsPerListing; j += 1) {
+            const startDate = formatDate(today);
+            today.setDate(today.getDate() + 1);
+            const endDate = formatDate(today);
 
-        const reservation = {
-          listing_id: i,
-          start_date,
-          end_date,
-        };
-        reservations.push(reservation);
-      }
+            const reservation = {
+              listing_id: i,
+              start_date: startDate,
+              end_date: endDate,
+            };
+            reservations.push(reservation);
+          }
+        }
+        return knex('reservations').insert(reservations);
+      })
+      .then(() => {
+        const daysOut = 90;
+        const listingDailyPrices = [];
+        for (let i = 1; i < subset + 1; i += 1) {
+          today = new Date();
+          for (let j = 0; j < daysOut; j += 1) {
+            const listingDailyPrice = {
+              listing_id: i,
+              cost_per_night: faker.random.number({
+                min: 0,
+                max: 500,
+                precision: 0.01,
+              }),
+              start_date: formatDate(today),
+            };
+            today.setDate(today.getDate() + 1);
+            listingDailyPrices.push(listingDailyPrice);
+          }
+        }
+        return knex('listing_daily_prices').insert(listingDailyPrices);
+      });
     };
-    return knex('reservations').insert(reservations);
-  })
-  .then(() => {
-    const daysOut = 90;
-    let listingDailyPrices = [];
-    for (let i = 1; i < numListings + 1; i += 1) {
-      let today = new Date();
-      for (let j = 0; j < daysOut; j += 1) {
-        let listingDailyPrice = {
-          listing_id: i,
-          cost_per_night: faker.random.number({
-            min: 0,
-            max: 500,
-            precision: 0.01,
-          }),
-          start_date: formatDate(today),
-        };
-        today.setDate(today.getDate() + 1);
-        listingDailyPrices.push(listingDailyPrice);
-      }
-    }
-    return knex('listing_daily_prices').insert(listingDailyPrices);
-  })
-)};
+  const factor = Math.ceil(numListings/subset);
+  await knex('listings').del()
+  await knex('reservations').del()
+  await knex('listing_daily_prices').del();
+  for (let i = 0; i < factor; i += 1) {
+    await createDbSubset();
+    console.log( `${i/factor*100}%`)
+  }
+};
--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,3 @@
-// const knex = require('knex');
-// const KnexOrm = require('knex-orm');
-
-// const Database = new KnexOrm(knex({
-//   client: 'mysql',
-//   connection: {
-//     user: 'root',
-//     password: '',
-//     databased: 'project_nomad_booking',
-//   },
-//   migrations: {
-//     directory: __dirname + '/db/migrations'
-//   },
-//   seeds: {
-//     directory: __dirname + '/db/seeds',
-//   },
-// }));
-
-// module.exports = {
-//   Database,
-// };
-
 const path = require('path');
 
 module.exports = {
@@ -32,43 +10,3 @@ module.exports = {
   migrations: { directory: path.join(__dirname, '/db/migrations') },
   seeds: { directory: path.join(__dirname, '/db/seeds') },
 };
-
-
-
-
-
-
-
-// class Listing extends Database.Model {
-//   static get primaryKey() {
-//     return 'id';
-//   }
-//   static get related() {
-//     return {
-//       reservations: this.hasMany('Reservation'),
-//       listingDailyPrices: this.hasMany('ListingDailyPrice'),
-//     };
-//   }
-// }
-
-// class Reservation extends Database.Model {
-//   static get related() {
-//     return {
-//       reservations: this.belongsTo('Listing'),
-//     };
-//   }
-// }
-
-// class ListingDailyPrice extends Database.Model {
-//   static get related() {
-//     return {
-//       listingDailyPrices: this.belongsTo('Listing'),
-//     };
-//   }
-// }
-
-
-// // Register Models to make them relatable without cross-referencing each other
-// Database.register(Listing);
-// Database.register(Reservation);
-// Database.register(ListingDailyPrice);
